459535ffdf3d20ce4963d20225560ae9fe76b745
anicalymus <anicalymus@gmail.com>
2017-12-11 14:56:09 -0800

    Review conversion to integer

--- analyzer.rb
+++ analyzer.rb
@@ -9,3 +9,11 @@ puts "Welcome #{first_name} #{last_name}, to the analyzer!"
 puts "Welcome #{first_name}, the length of your name is #{first_name.length} letters!"
 
 puts "#{first_name} reversed is #{first_name.reverse}."
+
+puts "Please enter your first number"
+first_number = gets.chomp
+
+puts "Please enter your second number"
+second_number = gets.chomp
+
+puts "The first number multiplied by the second number is: #{first_number.to_i * second_number.to_i}"

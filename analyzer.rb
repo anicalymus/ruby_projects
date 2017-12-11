14a3dc51ac616d87889419bf636ee93df896cc0b
anicalymus <anicalymus@gmail.com>
2017-12-11 15:31:25 -0800

    Review if/else statements and methods

--- analyzer.rb
+++ analyzer.rb
@@ -1,14 +1,21 @@
-puts "What is your first name?"
-first_name = gets.chomp
+def multiply(first_number, second_number)
+  first_number.to_f * second_number.to_f
+end
 
-puts "What is your last name?"
-last_name = gets.chomp
+def divide(first_number, second_number)
+  first_number.to_f / second_number.to_f
+end
 
-puts "Welcome #{first_name} #{last_name}, to the analyzer!"
+def subtract(first_number, second_number)
+  second_number.to_f - first_number.to_f
+end
 
-puts "Welcome #{first_name}, the length of your name is #{first_name.length} letters!"
+def mod(first_number, second_number)
+  first_number.to_f % second_number.to_f
+end
 
-puts "#{first_name} reversed is #{first_name.reverse}."
+puts "What do you want to do? 1) multiply 2) divide 3) subtract 4) find remainder"
+prompt = gets.chomp
 
 puts "Please enter your first number"
 first_number = gets.chomp
@@ -16,4 +23,31 @@ first_number = gets.chomp
 puts "Please enter your second number"
 second_number = gets.chomp
 
-puts "The first number multiplied by the second number is: #{first_number.to_i * second_number.to_i}"
+if prompt == "1"
+  puts "You have chosen to multiply #{first_number} with #{second_number}"
+  result = multiply(first_number, second_number)
+elsif prompt == "2"
+  puts "You have chosen to divide"
+  result = divide(first_number, second_number)
+elsif prompt == "3"
+  puts "You have chosen to subtract"
+  result = subtract(first_number, second_number)
+elsif prompt == "4"
+  puts "You have chosen to find the remainder"
+  result = mod(first_number, second_number)
+else
+  puts "You have made an invalid choice"
+end
+
+puts "The result is #{result}"
+
+# puts "Please enter your first number"
+# first_number = gets.chomp
+
+# puts "Please enter your second number"
+# second_number = gets.chomp
+
+
+
+
+
--- playground.rb
+++ playground.rb
@@ -1,4 +1,19 @@
-puts "Anica"
-puts "Lymus"
-puts "Anica" + " " + "Lymus"
+def multiply(first_number, second_number)
+  first_number.to_f * second_number.to_f
+end
+
+puts multiply(9, 2)
+
+def divide(first_number, second_number)
+  first_number.to_f / second_number.to_f
+end
+
+def subtract(first_number, second_number)
+  first_number.to_f - second_number.to_f
+end
+
+def mod(first_number, second_number)
+  first_number.to_f % second_number.to_f
+end
+
 
